ebf1f65308961cc5646f54933943089d3acf780b
JaviSrz <ljsuarez7@gmail.com>
2024-08-15 12:14:01 +0200

    Implementación del CRUD

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,18 @@
-export class CreateProductDto {}
+import { Type } from "class-transformer";
+import { IsNumber, IsOptional, IsString } from "class-validator";
+
+export class CreateProductDto {
+
+    @IsString()
+    name: string;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsNumber()
+    @Type(() => Number)
+    price: number;
+    
+
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+import {v4 as UuidV4} from 'uuid';
 
 @Injectable()
 export class ProductsService {
+
+  private products: Product[] = [];
+
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+
+    const {name, description, price} = createProductDto;
+
+    const newProduct = new Product(
+      UuidV4(),
+      name,
+      description,
+      price
+    );
+
+    this.products.push(newProduct);
+    return newProduct;
+
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.products;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  findOne(id: string): Product {
+    
+    const product = this.products.find(product => product.id === id);
+
+    if(!product){
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+
+    return product;
+
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  update(id: string, updateProductDto: UpdateProductDto) {
+
+    const {id:__, name, description, price } = updateProductDto; //Eliminamos el id en la desestructuración
+
+    const product = this.findOne(id);
+
+    product.updateWith({name, description, price}); //Se actualiza por referencia, lo hacemos así porque no tenemos DB
+
+    return product;
+
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  remove(id: string): Product {
+
+    const product = this.findOne(id);
+
+    this.products = this.products.filter(product => product.id !== id);
+
+    return product;
+
   }
 }
